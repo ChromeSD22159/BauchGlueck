97de94c6307d35f37467c0049f3d19e374af87fd
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-08-19 22:37:28 +0200

    KeyStorage

---
+++ shared/src/commonMain/kotlin/util/generateDeviceId.kt
@@ -0,0 +1,9 @@
+package util
+
+fun generateDeviceId(): String {
+    val chars = ('A'..'Z') + ('0'..'9')
+    val blocks = List(4) {
+        List(4) { chars.random() }.joinToString("")
+    }
+    return blocks.joinToString("-")
+}
