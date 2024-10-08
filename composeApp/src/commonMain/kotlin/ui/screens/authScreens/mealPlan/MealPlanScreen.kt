85ad5d0933e369280c722c3647a59744add076d0
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-10-08 15:01:16 +0200

    Refactor

--- composeApp/src/commonMain/kotlin/ui/screens/authScreens/mealPlan/MealPlanScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/authScreens/mealPlan/MealPlanScreen.kt
@@ -60,23 +60,21 @@ import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.vectorResource
-import org.lighthousegames.logging.logging
 import ui.components.theme.ScreenHolder
 import ui.components.theme.Section
-import ui.components.theme.button.TextButton
 import ui.components.theme.clickableWithRipple
 import ui.components.extentions.onLongPress
 import ui.components.extentions.sectionShadow
 import ui.components.theme.text.BodyText
 import ui.components.theme.text.HeadlineText
 import ui.components.extentions.toDigits
+import ui.components.theme.button.IconButton
 import ui.components.theme.text.FooterText
 import ui.navigations.Destination
 import ui.navigations.NavKeys
 import ui.navigations.getNavKey
 import ui.navigations.setNavKey
 import util.DateRepository
-import util.debugJsonHelper
 import util.parseToLocalDate
 import util.toLong
 import viewModel.FirebaseAuthViewModel
@@ -125,7 +123,10 @@ fun NavGraphBuilder.mealPlan(
                         .size(24.dp)
                         .clickableWithRipple {
                             navController.navigate(Destination.SearchRecipe.route)
-                            navController.setNavKey(NavKeys.Destination, Destination.MealPlanCalendar.route)
+                            navController.setNavKey(
+                                NavKeys.Destination,
+                                Destination.MealPlanCalendar.route
+                            )
                         },
                 )
 
@@ -420,7 +421,9 @@ fun NoMeal(
             text = "Keine Mahlzeit zugewiesen"
         )
 
-        TextButton(text = "add") {
+        IconButton(
+            resource = Res.drawable.ic_plus
+        ) {
             onClick()
         }
     }
@@ -541,14 +544,14 @@ fun ProgressComponent(
     }
 }
 
-sealed class Kcal(val kcal: Int) {
-    data object Junior : Kcal(800)
-    data object Mid : Kcal(1000)
-    data object SeniorKcal : Kcal(1200)
-    data class CustomKcal(val customKcal: Int) : Kcal(customKcal)
+sealed class KcalLevel(val kcal: Int) {
+    data object Junior : KcalLevel(800)
+    data object Mid : KcalLevel(1000)
+    data object SeniorKcalLevel : KcalLevel(1200)
+    data class CustomKcalLevel(val customKcal: Int) : KcalLevel(customKcal)
 }
 
-fun calculateKcalLevelSafely(operationTimestamp: Long): Kcal? {
+fun calculateKcalLevelSafely(operationTimestamp: Long): KcalLevel? {
     val currentTimestamp: Long = Clock.System.now().toEpochMilliseconds()
 
     val operationInstant = try {
@@ -566,8 +569,8 @@ fun calculateKcalLevelSafely(operationTimestamp: Long): Kcal? {
     val monthsElapsed = (currentDate.year - operationDate.year) * 12 + (currentDate.monthNumber - operationDate.monthNumber)
 
     return when {
-        monthsElapsed <= 3 -> Kcal.Junior
-        monthsElapsed <= 6 -> Kcal.Mid
-        else -> Kcal.SeniorKcal
+        monthsElapsed <= 3 -> KcalLevel.Junior
+        monthsElapsed <= 6 -> KcalLevel.Mid
+        else -> KcalLevel.SeniorKcalLevel
     }
 }
